a1dd398a0e1e77e3e76de19450199fda43542a7b
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-03-28 15:44:35 +0200

    Ajout calcul et affichage du total général

--- recap.php
+++ recap.php
@@ -26,7 +26,7 @@
                             "</tr>",
                         "</thead>",
                         "<tbody>";
-                
+                $totalGeneral = 0;
                 foreach($_SESSION['products'] as $index => $product){
                     echo "<tr>",
                             "<td>".$index."</td>",
@@ -35,9 +35,14 @@
                             "<td>".$product['qtt']."</td>",
                             "<td>".number_format($product['total'], 2, ",", "&nbsp;")."&nbsp;€"."</td>",
                         "</tr>";
+                    $totalGeneral+= $product['total'];
                 }
-                echo "</tbody>",
-                    "</table>";
+                echo "<tr>",
+                        "<td colspan=4>Total général : </td>",
+                        "<td><strong>".number_format($totalGeneral, 2, ",", "&nbsp;€")."&nbsp;€</strong></td>",
+                     "</tr>",
+                    "</tbody>",
+                "</table>";
             }
         ?>
     </body>
