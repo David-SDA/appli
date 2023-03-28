3a3ebd068cb71d46096c23aa5923e8016dead480
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-03-28 15:38:30 +0200

    Ajout affichage dynamique des produits

--- recap.php
+++ recap.php
@@ -15,7 +15,29 @@
                 echo "<p>Aucun produit en session...</p>";
             }
             else{
-
+                echo "<table>",
+                        "<thead>",
+                            "<tr>",
+                                "<th>#</th>",
+                                "<th>Nom</th>",
+                                "<th>Prix</th>",
+                                "<th>Quantité</th>",
+                                "<th>Total</th>",
+                            "</tr>",
+                        "</thead>",
+                        "<tbody>";
+                
+                foreach($_SESSION['products'] as $index => $product){
+                    echo "<tr>",
+                            "<td>".$index."</td>",
+                            "<td>".$product['name']."</td>",
+                            "<td>".number_format($product['price'], 2, ",", "&nbsp;")."&nbsp;€"."</td>",
+                            "<td>".$product['qtt']."</td>",
+                            "<td>".number_format($product['total'], 2, ",", "&nbsp;")."&nbsp;€"."</td>",
+                        "</tr>";
+                }
+                echo "</tbody>",
+                    "</table>";
             }
         ?>
     </body>
