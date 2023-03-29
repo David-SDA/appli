94cb1836a4437d43562abf47c298eb8a0bbebf03
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-03-29 14:27:00 +0200

    Ajout switch pour faire des fonctionnalités

--- traitement.php
+++ traitement.php
@@ -1,28 +1,35 @@
 <?php
     session_start();
 
-    if(isset($_POST['submit'])){
+    if(isset($_GET['action'])){
 
-        $name = filter_input(INPUT_POST, "name", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-        $price = filter_input(INPUT_POST, "price", FILTER_VALIDATE_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
-        $qtt = filter_input(INPUT_POST, "qtt", FILTER_VALIDATE_INT);
+        switch($_GET['action']){
+            //* ----------------AJOUTER UN PRODUIT------------------
+            case "add" :
+                if(isset($_POST['submit'])){
 
-        if($name && $price && $qtt){
+                    // filtrer les inputs du formulaire
+                    $name = filter_input(INPUT_POST, "name", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+                    $price = filter_input(INPUT_POST, "price", FILTER_VALIDATE_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+                    $qtt = filter_input(INPUT_POST, "qtt", FILTER_VALIDATE_INT);
             
-            $product = [
-                "name" => $name,
-                "price" => $price,
-                "qtt" => $qtt,
-                "total" => $price*$qtt
-            ];
-
-            $_SESSION['products'][] = $product;
-            $_SESSION['message'] = "Réussi !";
+                    if($name && $price && $qtt){
+                        
+                        $product = [
+                            "name" => $name,
+                            "price" => $price,
+                            "qtt" => $qtt,
+                            "total" => $price*$qtt
+                        ];
+            
+                        $_SESSION['products'][] = $product;
+                        $_SESSION['message'] = "Réussi !";
+                    }
+                    else{
+                        $_SESSION['message'] = "Échec !";
+                    }
+                }
         }
-        else{
-            $_SESSION['message'] = "Échec !";
-        }
-
     }
 
     header("Location:index.php");
